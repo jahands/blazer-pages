c93948bc19d2a4f8ce58f9681fa6af5b51768fa8
Jacob Hands <jacob@gogit.io>
2023-04-28 06:46:06 -0500

    fix version

--- proj7/build.sh
+++ proj7/build.sh
@@ -2,5 +2,5 @@
 curl -sSL https://dot.net/v1/dotnet-install.sh > dotnet-install.sh
 chmod +x dotnet-install.sh
 ./dotnet-install.sh -c 7.0 -InstallDir ./dotnet7
-./dotnet6/dotnet --version
-./dotnet6/dotnet publish -c Release -o output
+./dotnet7/dotnet --version
+./dotnet7/dotnet publish -c Release -o output
