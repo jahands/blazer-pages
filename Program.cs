73fd5fffeaaf84ceea34414fd526d5b6ef4de032
Jacob Hands <jacob@gogit.io>
2023-04-28 06:53:44 -0500

    regen as proj 6

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using proj7;
+using proj6;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
